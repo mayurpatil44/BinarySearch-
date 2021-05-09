955c76113bac1b8b138bf7310008102b3179d207
mayurpatil44 <mayurpatil44@hotmail.com>
2021-05-09 15:59:25 +0530

    Ability to create binary search tree

---
+++ src/main/java/com/binarysearch/BinarySearchTree.java
@@ -0,0 +1,32 @@
+package com.binarysearch;
+
+public class BinarySearchTree<K extends Comparable<K>> {
+
+    private BinaryNode<K> root;
+
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+    }
+
+    private BinaryNode<K> addRecursively(BinaryNode<K> current, K key) {
+        if (current == null)
+            return new BinaryNode<>(key);
+        int result = key.compareTo(current.key);
+        if (result == 0)
+            return current;
+        if (result < 0)
+            current.left = addRecursively(current.left, key);
+        else
+            current.right = addRecursively(current.right, key);
+        return current;
+    }
+
+    public int getSize() {
+        return this.getSizeRecursively(root);
+    }
+
+    private int getSizeRecursively(BinaryNode<K> current) {
+        return (current == null) ? 0
+                : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+    }
+}
---
+++ src/test/java/com/binarysearch/BinarySearchTreeTest.java
@@ -0,0 +1,15 @@
+package com.binarysearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinarySearchTreeTest {
+    @Test
+    public void WhenNmbersAreAddedToBinarySearchTreeShouldReturnSize() {
+        BinarySearchTree<Integer> binaryTree = new BinarySearchTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        Assert.assertEquals(3, binaryTree.getSize());
+    }
+}
